61f56f73b58486a5c5342517a351c1baaf5b5402
Taylor McMillen <60412320+TaylorNMcMillen@users.noreply.github.com>
2023-04-25 20:56:22 -0300

    Update IN Operator Practice.sql

--- IN Operator Practice.sql
+++ IN Operator Practice.sql
@@ -1,10 +1,11 @@
--- SELECT *
--- FROM Customers
--- WHERE state = 'VA' OR state = 'GA' or state = 'FL'
 -- Isn't there an easier way to query this?
+SELECT *
+FROM Customers
+WHERE state = 'VA' OR state = 'GA' or state = 'FL'
 
--- WHERE state IN ('VA','FL','GA')
--- Returns the same as above, is easier to read and understand, and faster to write
+--Returns the same as above, is easier to read and understand, and faster to write
+WHERE state IN ('VA','FL','GA')
+
 
 SELECT *
 FROM products
