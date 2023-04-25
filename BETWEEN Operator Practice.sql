f9f8d97cf4e81093f5c49277c47874f4886adc3f
Taylor McMillen <60412320+TaylorNMcMillen@users.noreply.github.com>
2023-04-25 20:54:56 -0300

    Update BETWEEN Operator Practice.sql

--- BETWEEN Operator Practice.sql
+++ BETWEEN Operator Practice.sql
@@ -1,10 +1,11 @@
--- SELECT *
--- FROM customers
--- WHERE points >= 1000 AND points <= 3000
+SELECT *
+FROM customers
+WHERE points >= 1000 AND points <= 3000
 -- isn't there an easier way to query this?
 
--- WHERE points BETWEEN 1000 AND 3000
 -- faster, easier to understand and read 
+WHERE points BETWEEN 1000 AND 3000
+
 
 SELECT *
 FROM customers
