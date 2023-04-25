c9ead8984fb3e5bcb06af1e8b67431596aa75070
Taylor McMillen <60412320+TaylorNMcMillen@users.noreply.github.com>
2023-04-25 20:56:38 -0300

    Update INNER JOINS practice.sql

--- INNER JOINS practice.sql
+++ INNER JOINS practice.sql
@@ -1,12 +1,12 @@
--- SELECT order_id, o.customer_id, first_name, last_name
--- FROM orders o
--- INNER JOIN customers c ON o.customer_id = c.customer_id
+SELECT order_id, o.customer_id, first_name, last_name
+FROM orders o
+INNER JOIN customers c ON o.customer_id = c.customer_id
 
 SELECT oi.order_id, oi.product_id, p.name, quantity, oi.unit_price
 FROM order_items oi
 JOIN products p ON oi.product_id = p.product_id
 
--- USE sql_store;
--- SELECT *
--- FROM sql_store.order_items oi
--- JOIN sql_inventory.products p ON oi.product_id = p.product_id
+USE sql_store;
+SELECT *
+FROM sql_store.order_items oi
+JOIN sql_inventory.products p ON oi.product_id = p.product_id
