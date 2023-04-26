c6942e8836a7344fe79937f05a0d0cf42b0ad2b7
Taylor McMillen <60412320+TaylorNMcMillen@users.noreply.github.com>
2023-04-25 21:00:37 -0300

    Update LIMIT practice.sql

--- LIMIT practice.sql
+++ LIMIT practice.sql
@@ -1,8 +1,8 @@
--- SELECT *
--- FROM customers
--- LIMIT 6, 3
+SELECT *
+FROM customers
+LIMIT 6, 3
 
--- The 6 is what is known as an offset, which will skip that amount of records, then return the 3 records that come nextorders
+The 6 is what is an offset, which will skip that amount of records, then return the 3 records that come nextorders
 
 SELECT *
 FROM customers
